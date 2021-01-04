abd9e485969b7250ba2274c0183533d4fedb7c26
LynxN1 <47945306+LynxN1@users.noreply.github.com>
2021-01-04 16:25:34 +0200

    1.5

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from src import service
+from colorama import init
+from termcolor import colored
+from src import __version__, __author__
+from src.service import ServiceApp
+import os
+
+init()
 
 if __name__ == '__main__':
-    print("Start service")
-    service.run()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    logo = open("src/draw/logo.txt", "r").read().replace("v?", __version__).replace("a?", __author__)
+    print(colored(logo, "green"))
+    ServiceApp().run()
--- src/service.py
+++ src/service.py
@@ -2,14 +2,20 @@ import json
 import os
 import random
 import threading
+import time
 import traceback
 from multiprocessing import Pool
 from pathlib import Path
+
+from termcolor import colored
 
 import amino
 
 with open(os.getcwd() + "/src/devices/devices.txt", "r") as file:
     devices = file.readlines()
+
+with open(os.getcwd() + "/src/activity/comments.txt", "r") as comments_file:
+    comments = comments_file.readlines()
 
 path = Path(os.getcwd() + '/src/accounts/bots.json')
 accounts = json.loads(path.read_text(encoding='utf-8'))
@@ -22,7 +28,8 @@ class Login:
             password = self.get_password(email)
             client = amino.Client()
             try:
-                print(f"[Login][{email}]: {client.login(email=email, password=password)}")
+                print(f"Authorization {email}...")
+                client.login(email=email, password=password)
                 break
             except amino.exceptions.ActionNotAllowed:
                 print("Change device_id")
@@ -46,6 +53,7 @@ class Login:
             except amino.exceptions.VerificationRequired as verify:
                 print(verify)
                 input("Confirm your account and press ENTER...")
+        print("Authorization was successful")
         self.save_auth_data(email, password)
         return client
 
@@ -100,62 +108,65 @@ class Login:
             json.dump(auth_data, f)
 
 
-def run():
-    is_login = input("Choose a community or enter a community ID?? (1/2): ")
-    if is_login == "1":
-        client = Login().login()
-        sub_clients = client.sub_clients(start=0, size=100)
+class Community:
+    def __init__(self, client):
+        self.client = client
+
+    def select(self):
+        sub_clients = self.client.sub_clients(start=0, size=100)
         for x, name in enumerate(sub_clients.name, 1):
             print(f"{x}. {name}")
         choice_sub_client = input("Enter community number: ")
-        com_id = sub_clients.comId[int(choice_sub_client) - 1]
-    else:
-        com_id = input("Enter community ID: ")
-    while True:
-        try:
-            choice = input("\n"
-                           "0. Change Community\n"
-                           "1. PlayLottery\n"
-                           "2. SendCoins\n"
-                           "3. PlayQuiz\n"
-                           "4. LikeBlog\n"
-                           "5. Unfollow\n"
-                           "6. LikeRecentBlogs\n"
-                           ">>> ")
-            if choice == "1":
-                PlayLottery(com_id)
-                print("[PlayLottery]: Finish.")
-            elif choice == "2":
-                SendCoins(com_id)
-                print("[SendCoins]: Finish.")
-            elif choice == "3":
-                PlayQuiz(com_id)
-                print("[PlayQuiz]: Finish.")
-            elif choice == "4":
-                LikeBlog(com_id)
-                print("[LikeBlog]: Finish.")
-            elif choice == "5":
-                Unfollow(com_id)
-                print("[Unfollow]: Finish.")
-            elif choice == "6":
-                LikeRecentBlogs(com_id)
-                print("[LikeRecentBlogs]: Finish.")
-            elif choice == "0":
-                com_id = input("Community ID: ")
-                print("Community ID changed")
-        except:
-            print(traceback.format_exc())
+        return sub_clients.comId[int(choice_sub_client) - 1]
 
 
-class PlayLottery:
-    """
-    Play the lottery from all accounts in bots.json
-    """
-    def __init__(self, com_id: str):
-        self.com_id = com_id
-        self.client = amino.Client()
-        pool = Pool(10)
-        pool.map(self.play_lottery, accounts.items())
+class ServiceApp:
+    def __init__(self):
+        self.back = False
+        self.client = Login().login()
+        self.com_id = Community(self.client).select()
+        self.object_id = None
+
+    def run(self):
+        while True:
+            try:
+                logo = open("src/draw/menu.txt", "r").read()
+                print(colored(logo, "cyan"))
+                choice = input("Enter the number >>> ")
+                if choice == "1":
+                    pool = Pool(10)
+                    pool.map(self.play_lottery, accounts.items())
+                    print("[PlayLottery]: Finish.")
+                elif choice == "2":
+                    blog_link = input("Blog link: ")
+                    self.object_id = self.client.get_from_code(str(blog_link.split('/')[-1])).objectId
+                    pool = Pool(10)
+                    pool.map(self.send_coins, accounts.items())
+                    print("[SendCoins]: Finish.")
+                elif choice == "3":
+                    quiz_link = input("Quiz link: ")
+                    self.object_id = self.client.get_from_code(str(quiz_link.split('/')[-1])).objectId
+                    self.play_quiz()
+                    print("[PlayQuiz]: Finish.")
+                elif choice == "4":
+                    blog_link = input("Blog link: ")
+                    self.object_id = self.client.get_from_code(str(blog_link.split('/')[-1])).objectId
+                    pool = Pool(10)
+                    pool.map(self.like_blog, accounts.items())
+                    print("[LikeBlog]: Finish.")
+                elif choice == "5":
+                    self.unfollow()
+                    print("[Unfollow]: Finish.")
+                elif choice == "6":
+                    threading.Thread(target=self.like_recent_blogs).start()
+                    input("\nPress ENTER to stop...\n")
+                    self.back = True
+                    print("[LikeRecentBlogs]: Finish.")
+                elif choice == "0":
+                    self.com_id = Community(self.client).select()
+                    print("Community changed")
+            except:
+                print(traceback.format_exc())
 
     def play_lottery(self, account: dict):
         email = account[0]
@@ -174,19 +185,6 @@ class PlayLottery:
             print(f"[{email}][Exception]:\n{e}")
             return
 
-
-class SendCoins:
-    """
-    Sends coins from accounts in bots.json to the selected blog
-    """
-    def __init__(self, com_id: str):
-        self.com_id = com_id
-        self.client = amino.Client()
-        blog_link = input("Blog link: ")
-        self.blog_id = self.client.get_from_code(str(blog_link.split('/')[-1])).objectId
-        pool = Pool(10)
-        pool.map(self.send_coins, accounts.items())
-
     def send_coins(self, account: dict):
         email = account[0]
         password = account[1]
@@ -201,7 +199,7 @@ class SendCoins:
             return
 
         try:
-            print(f"[{email}][send_coins]: {sub_client.send_coins(coins=coins, blogId=self.blog_id)}")
+            print(f"[{email}][send_coins]: {sub_client.send_coins(coins=coins, blogId=self.object_id)}")
         except amino.exceptions.NotEnoughCoins:
             print(f"[{email}][NotEnoughCoins][coins {coins}]")
             return
@@ -210,24 +208,12 @@ class SendCoins:
             return
         print(f"[{email}]: {coins} монет отправлено.")
 
-
-class PlayQuiz:
-    """
-    Pass the quiz for maximum results
-    """
-    def __init__(self, com_id: str):
-        self.com_id = com_id
-        self.client = Login().login()
-        self.quiz_link = input("Link: ")
-        self.play_quiz()
-
     def play_quiz(self):
         questions_list = []
         answers_list = []
 
         subclient = amino.SubClient(comId=self.com_id, profile=self.client.profile)
-        quiz_id = subclient.get_from_code(str(self.quiz_link.split('/')[-1])).objectId
-        quiz_info = subclient.get_blog_info(quizId=quiz_id).json
+        quiz_info = subclient.get_blog_info(quizId=self.object_id).json
         quiestions = quiz_info["blog"]["quizQuestionList"]
         total_questions = quiz_info["blog"]["extensions"]["quizTotalQuestionCount"]
 
@@ -237,8 +223,8 @@ class PlayQuiz:
             answers = question["extensions"]["quizQuestionOptList"]
             for answer in answers:
                 answer_id = answer["optId"]
-                subclient.play_quiz(quizId=quiz_id, questionIdsList=[question_id], answerIdsList=[answer_id])
-                latest_score = subclient.get_quiz_rankings(quizId=quiz_id).profile.latestScore
+                subclient.play_quiz(quizId=self.object_id, questionIdsList=[question_id], answerIdsList=[answer_id])
+                latest_score = subclient.get_quiz_rankings(quizId=self.object_id).profile.latestScore
                 if latest_score > 0:
                     print(f"[quiz][{x}/{total_questions}]: Answer found!")
                     questions_list.append(question_id)
@@ -246,26 +232,13 @@ class PlayQuiz:
 
         if "quizInBestQuizzes" in quiz_info["blog"]["extensions"]:
             for i in range(2):
-                subclient.play_quiz(quizId=quiz_id, questionIdsList=questions_list, answerIdsList=answers_list,
+                subclient.play_quiz(quizId=self.object_id, questionIdsList=questions_list, answerIdsList=answers_list,
                                     quizMode=i)
         else:
-            subclient.play_quiz(quizId=quiz_id, questionIdsList=questions_list, answerIdsList=answers_list, quizMode=0)
+            subclient.play_quiz(quizId=self.object_id, questionIdsList=questions_list, answerIdsList=answers_list, quizMode=0)
 
         print(f"[quiz]: Passed the quiz!")
-        print(f"[quiz]: Score: {subclient.get_quiz_rankings(quizId=quiz_id).profile.highestScore}")
-
-
-class LikeBlog:
-    """
-    Accounts in bots.json like the selected blog
-    """
-    def __init__(self, com_id: str):
-        self.com_id = com_id
-        self.client = amino.Client()
-        blog_link = input("Blog link: ")
-        self.blog_id = self.client.get_from_code(str(blog_link.split('/')[-1])).objectId
-        pool = Pool(10)
-        pool.map(self.like_blog, accounts.items())
+        print(f"[quiz]: Score: {subclient.get_quiz_rankings(quizId=self.object_id).profile.highestScore}")
 
     def like_blog(self, account: dict):
         email = account[0]
@@ -275,19 +248,9 @@ class LikeBlog:
             return
         sub_client = amino.SubClient(comId=self.com_id, profile=self.client.profile)
         try:
-            print(f"[{email}][like]: {sub_client.like_blog(blogId=self.blog_id)}")
+            print(f"[{email}][like]: {sub_client.like_blog(blogId=self.object_id)}")
         except amino.exceptions.RequestedNoLongerExists:
-            print(f"[{email}][like]: {sub_client.like_blog(wikiId=self.blog_id)}")
-
-
-class Unfollow:
-    """
-    Unfollow from everyone the user is following
-    """
-    def __init__(self, com_id: str):
-        self.com_id = com_id
-        self.client = Login().login()
-        self.unfollow()
+            print(f"[{email}][like]: {sub_client.like_blog(wikiId=self.object_id)}")
 
     def unfollow(self):
         subclient = amino.SubClient(comId=self.com_id, profile=self.client.profile)
@@ -304,19 +267,6 @@ class Unfollow:
                     print(f"[{iteration}]: Unfollow")
                     subclient.unfollow(userId=user_id)
 
-
-class LikeRecentBlogs:
-    def __init__(self, com_id: str):
-        self.com_id = com_id
-        self.client = Login().login()
-        self.back = False
-        self.run()
-
-    def run(self):
-        threading.Thread(target=self.like_recent_blogs).start()
-        input("\nPress ENTER to stop...\n")
-        self.back = True
-
     def like_recent_blogs(self):
         subclient = amino.SubClient(comId=self.com_id, profile=self.client.profile)
         old_blogs = []
@@ -328,9 +278,14 @@ class LikeRecentBlogs:
                     try:
                         try:
                             subclient.like_blog(blogId=blog_id)
+                            subclient.comment(message=random.choice(comments), blogId=blog_id)
+                            time.sleep(2.5)
                         except amino.exceptions.RequestedNoLongerExists:
                             subclient.like_blog(wikiId=blog_id)
+                            subclient.comment(message=random.choice(comments), wikiId=blog_id)
+                            time.sleep(2.5)
                         print("Like")
                     except Exception as e:
                         print(e)
                     old_blogs.append(blog_id)
+            time.sleep(10)
